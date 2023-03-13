95ee0ed5a6db0220c57a1cfadb5dbaacc3e7f43e
Luís Alves <luiscnalves@gmail.com>
2023-03-13 07:29:28 +0000

    Added mobile format

--- js/cool_menu.js
+++ js/cool_menu.js
@@ -9,7 +9,7 @@ var sse1 = function () {
             var m = document.getElementById('sses1');
             if(!m) return;
             var ul = m.getElementsByTagName("ul")[0];
-            m.style.width = ul.offsetWidth+1+"px";
+            m.style.width = "100%";
             var items = m.getElementsByTagName("li");
             var a = m.getElementsByTagName("a");
 
